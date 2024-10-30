a898590a2269abf9996538df01c38efcc7d178df
ahadiat <ahadiatsudirman045@gmail.com>
2024-10-30 12:40:54 +0800

    add githubactions

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import Homepage from './components/pages/Homepage'
+
+const App = () => {
+  return (
+
+    <div> 
+  <Homepage />
+  
+
+
+
+  </div>
+
+  )
+}
+
+export default App
