ffffd87d7361188976203782c06e2ed425241ca1
ahadiat <ahadiatsudirman045@gmail.com>
2024-11-05 08:32:52 +0800

    config fixing and path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/out/',
+  base: '',
   build: {
     outDir: 'out', // Change this to 'out'
     assetsDir: 'assets', // Ensure assets are placed correctly
